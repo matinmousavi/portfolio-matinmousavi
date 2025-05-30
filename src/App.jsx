8c76d25749497d0fda56efd51fc350bddcebeecd
matinmousavi <matinmousavi5049@gmail.com>
2025-05-31 01:26:43 +0330

    Add Tailwind CSS and Vazir font integration

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+	<h1 className="flex justify-center items-center bg-[#101214] text-[#50B07C] h-screen text-3xl font-normal">سلام قراره به زودی در این صفحه با من بیشتر آشنا بشی😊</h1>
+  )
+}
+
+export default App
